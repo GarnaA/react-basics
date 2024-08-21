a6fa7558a9a9db9e0c7f8440c3579819c8f7e72b
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-08-21 11:51:51 +0200

    feat: add possibility to handle additional props in Greeting component

--- react_props/src/App.jsx
+++ react_props/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 
-function Greeting(props) {
+/*function Greeting(props) {
   const { name } = props;
   return (
     <h1 className="greeting">
-      Hello <i className="emphasize">(name)</i>. Welcome!
+      Hello <i className="emphasize">{name}</i>, Welcome!
     </h1>
   );
 }
@@ -12,6 +12,27 @@ function Greeting(props) {
 function App() {
   return (
     <Greeting name="Everyone" style={{ border: "solid red"}} aria-hidden></Greeting>
+  );
+}
+
+export default App;
+*/
+
+
+function Greeting(props) {
+  
+  const { name, ...rest } = props;
+  console.log(rest);
+  return (
+    <h1 className="greeting" {...rest}>
+      Hello <i className="emphasize">{name}</i>, Welcome!
+    </h1>
+  );
+}
+
+function App() {
+  return (
+    <Greeting name="Everyone" style={{ border: "solid red"}} aria-hidden={true} id="some-id"></Greeting>
   );
 }
 
