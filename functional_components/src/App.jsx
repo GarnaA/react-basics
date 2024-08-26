763192f1d9d5a63d2426b4ca0db6974491941e8e
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-08-26 09:28:05 +0200

    feat: add class component with state management and toggle functionality

--- functional_components/src/App.jsx
+++ functional_components/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Component } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  //field to store reactive data
+  state = { data: false };
+  
+  //regular class method to update state
+  toggle = () => this.setState({ data: !this.state.data });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  //markup
+  render(){
+    const { data } = this.state;
+    return (
+      <main>
+        <h1>Class component with State</h1>
+        <p>{ data === true ? 'True' : 'False'}</p>
+        <button onClick={this.toggle}> Toggle </button>
+      </main>
+    );
+  }
 }
 
-export default App
+export default App;
